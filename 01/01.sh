2bdd83019c3237e4723e9f07acad9efb7e8f761f
Joao Carreira <jfmcarreira@gmail.com>
2023-12-09 02:58:00 +0000

    feat(code): add code for day 01

---
+++ 01/01.sh
@@ -0,0 +1,28 @@
+#!/opt/homebrew/bin/bash
+[ $# -lt 1 ] && echo "Missing input!" && exit 1
+
+function to_lower() {
+    awk '{print tolower($0)}'
+}
+
+function replace_text_as_numbers() {
+    sed 's/one/o1e/g; s/two/t2/g; s/three/t3e/g; s/four/f4r/g; s/five/f5e/g; s/six/s6x/g; s/seven/s7n/g; s/eight/e8t/g; s/nine/n9e/g'
+}
+
+function remove_letters() {
+    sed 's#[a-zA-Z]##g'
+}
+
+function duplicate_line() {
+    sed 's/.*/&&/'
+}
+
+function fetch_first_and_last() {
+    sed -n 's/\(.\).*\(.\)/\1\2/p'
+}
+
+function sum() {
+    awk '{sum += $1 } END { print sum}'
+}
+
+cat $1 | to_lower | replace_text_as_numbers | remove_letters  | duplicate_line | fetch_first_and_last  | sum
---
+++ README.md
@@ -0,0 +1,4 @@
+# Code used to submit Advent of Code 2023
+
+# Dependencies
+- Shell scripts used the Homebrew version of Bash 5.2 (instead of the one from MacOS)
